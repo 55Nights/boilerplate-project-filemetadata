2745f49ab894dd2055886de6eb3a2dae4d0e8409
55Nights <1905587@students.kca.ac.ke>
2023-09-02 13:44:47 +0300

    file uploads

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer')
 require('dotenv').config()
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+// Create a Multer instance and specify the destination for uploaded files
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/')
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname)
+  }
+});
+
+const upload = multer({storage})
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -13,6 +25,13 @@ app.get('/', function (req, res) {
 
 
 
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  res.json({
+    name: req.file.filename,
+    "type": req.file.mimetype,
+    "size":req.file.size
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
